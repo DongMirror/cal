75d428da4d2b530687969242df021dd9727e9102
DongminLee <ldm030475@naver.com>
2024-07-15 13:02:09 +0900

    feat: 계산기 인터페이스 구축

--- src/main/java/com/study/Calculator.java
+++ src/main/java/com/study/Calculator.java
@@ -1,13 +1,8 @@
 package com.study;
 
-public class Calculator {
+public interface Calculator {
+    
+    void add(int a, int b);
 
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-
+    void sub(int a, int b);
 }
