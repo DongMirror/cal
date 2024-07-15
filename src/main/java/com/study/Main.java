07013a067bf1b0f3908d9a08311a188e8fd0e865
HyeonSeon9 <a01062704312@gmail.com>
2024-07-15 11:16:01 +0900

    Feat: 덧셈 뺼셈 계산기 추가

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -2,6 +2,10 @@ package com.study;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculator calculator = new Calculator();
+
+        System.out.println(calculator.add(1,2));
+        System.out.println(calculator.subtract(1,2));
+
     }
 }
