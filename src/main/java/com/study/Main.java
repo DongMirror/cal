cd6071344dea5bfa2f62e1760e8fe93146805230
DongminLee <ldm030475@naver.com>
2024-07-15 13:05:34 +0900

    feat: 계산기 구현

--- src/main/java/com/study/Main.java
+++ src/main/java/com/study/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new CalculatorImpl();
 
         System.out.print("숫자 입력 : ");
         int num1 = scanner.nextInt();
@@ -15,5 +16,15 @@ public class Main {
         System.out.print("숫자 입력 : ");
         int num2 = scanner.nextInt();
 
+        switch (oper) {
+            case "+":
+                calculator.add(num1, num2);
+                break;
+            case "-":
+                calculator.sub(num1, num2);
+                break;
+            default:
+                System.out.println("잘못된 연산자 입니다.");
+        }
     }
 }
